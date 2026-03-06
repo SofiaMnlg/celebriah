aef3a328a8e44f794b6b5180b1a13634b91ed9c9
Bonaasm <sofiamanullang0@gmail.com>
2026-03-06 10:05:41 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,4 +41,4 @@ app.get("/", (req, res) => {
 
 // Jalankan server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0",() => console.log(`🚀 Server running on port ${PORT}`));
